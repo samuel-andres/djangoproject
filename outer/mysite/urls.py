c63d48e4588b5b53ba5015297dd6ce1a462f6678
samucancld <samuel5848@gmail.com>
2022-07-10 06:01:23 -0300

    adding a redirect from home

--- outer/mysite/urls.py
+++ outer/mysite/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
+
+
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path("", views.home, name="home"),
 ]
